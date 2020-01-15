06c974d87dc03f17be69655692ffa148c53d7c46
Владен <vladlen11.89@rambler.ru>
2020-01-15 22:36:43 +0200

    about us

--- src/js/sources/slick-activate.js
+++ src/js/sources/slick-activate.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var slickVarBonuses = {
     arrows: true,
     infinite: true,
-    dots: true,
+    dots: false,
     slidesToShow: 1,
     responsive: [{
       breakpoint: 2560,
@@ -16,23 +16,7 @@ $(document).ready(function() {
     }
     ]
   };
-
-  runSlickForever = function() {
-    $('.c_slider').slick(slickVarBonuses);
-  };
-
-  calcSlides = function() {
-    $('.c_slider__counter').remove();
-    var totalSlides   = $('.slick-dots li').length,
-    indexCurrentSlide = $('.slick-dots li.slick-active').index() + 1;
-    $('.c_slider').append("<div class='c_slider__counter'>0<span id='slider-current'>" + indexCurrentSlide + "</span>/0<span id='totalSlides'>" + totalSlides + "</span></div>");
-  };
-  runSlickForever();
-  calcSlides();
-  $("body").click(calcSlides);
-  $("body").mousemove(calcSlides);
-  $(".c_slider").on("tap", calcSlides);
-  $(".c_slider").on("swipe", calcSlides);
+  $('.c_slider').slick(slickVarBonuses);
 
 });
 
