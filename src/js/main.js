0c38af1ee4430cd0da7d3eb292840b50e0a7ad2e
Владен <vladlen11.89@rambler.ru>
2020-01-17 21:01:48 +0200

    desctop

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,36 @@
 //= sources/slick-activate.js
 //= sources/jquery.magnific-popup.min.js
 
-//$(document).ready(function() {
-//    $('.popup-link').magnificPopup({
-//        type: 'image'
-//        // other options
-//    });
-//});
+$(document).ready(function() {
+    $('.popup-link').magnificPopup({
+        type: 'image'
+        // other options
+    });
+});
+
+var textarea = $('#textarea');
+
+textarea.blur(function(){
+    tmpval = $(this).val();
+    if(tmpval == '') {
+        $(this).addClass('empty');
+        $(this).removeClass('not-empty');
+        $('.textarea-wrap').addClass('empty');
+        $('.textarea-wrap').removeClass('not-empty');
+    } else {
+        $(this).addClass('not-empty');
+        $(this).removeClass('empty');
+        $('.textarea-wrap').addClass('not-empty');
+        $('.textarea-wrap').removeClass('empty');
+    }
+});
+textarea.focusin(function(){
+    $('.textarea-wrap').addClass('not-empty');
+    $('.textarea-wrap').removeClass('empty');
+});
+textarea.focusout(function(){
+
+    $('.textarea-wrap').addClass('empty');
+    $('.textarea-wrap').removeClass('not-empty');
+});
+
